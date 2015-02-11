efeefe9eea066871edfac4604a9c185116cd9747
Steve Brown <contactstevebrown@gmail.com>
2015-02-11 18:06:32 +0000

    wip

--- js/triangles.js
+++ js/triangles.js
@@ -2,46 +2,60 @@ var stage = new Kinetic.Stage({
     container: 'container',
     width: window.innerWidth,
     height: window.innerHeight
-})
+});
 
-var layer = new Kinetic.Layer();
+var container = document.getElementById('container'),
+    layer = new Kinetic.Layer();
 
 for (var i = 0; i < 3; i++) {
-
     createTriangleGroup(i)
-
 }
 
 stage.add(layer);
 
 function createTriangleGroup(i) {
 
-    var x = 0;
     var leftTriangle = new Kinetic.Shape({
+
+        test: Math.random(),
 
         drawFunc: function (ctx) {
             var img = document.createElement('img'),
                 self = this,
-                m = i * 100;
+                xo = -34,           // x overlap
+                w = 248,            // triangle width
+                x = (i * w) + w;    // x position
             img.src = 'img/pattern.jpg';
-            
+
             img.onload = function () {
                 ctx.save();
                 ctx.beginPath();
-                ctx.moveTo(0 + m, 0);
-                ctx.lineTo(207 + m, 140);
-                ctx.lineTo(-34 + m, 248);
-                ctx.lineTo(0 + m, 0);
+                ctx.moveTo(0 + x, 0);
+                ctx.lineTo(207 + x, 140);
+                ctx.lineTo(-34 + x, w);
+                ctx.lineTo(0 + x, 0);
                 ctx.clip();
-                ctx.drawImage(img, 0 + m, 0, img.width, img.height);
+                ctx.drawImage(img, (0 + x) + xo, 0, img.width, img.height);
                 ctx.stroke();
                 ctx.closePath();
                 ctx.fillStrokeShape(self);
                 ctx.restore();
             }
         }
-    }).on('mousedown', function (e) {
-        console.log('id',e.target._id);
+    })
+
+    .on('mousedown', function (e) {
+        console.log(e.target.attrs.test);
+    })
+    .on('mouseover', function (e) {
+        this.setStroke('red');
+        layer.draw();
+        container.style.cursor = 'pointer';
+    })
+    .on('mouseout', function (e) {
+        this.setStroke('none');
+        layer.draw();
+            container.style.cursor = 'auto';
     });
 
     var rightTriangle = new Kinetic.Shape({
@@ -66,55 +80,9 @@ function createTriangleGroup(i) {
             }
         }
     });
-
-    console.log('adding triangle')
-
+    
     layer.add(leftTriangle);
-    layer.draw();
 
 };
-
-
-//function insideFor(i) {
-//
-//    var circle_outer = new Kinetic.Shape({
-//        drawFunc: function (ctx) {
-//            var img = document.createElement('img'),
-//                self = this;
-//            img.src = 'img/pattern.jpg';
-//
-//            img.onload = function () {
-//                ctx.save();
-//                ctx.translate(100 * i,100)
-//                ctx.beginPath();
-//                ctx.moveTo(0, 0);
-//                ctx.lineTo(207, 140);
-//                ctx.lineTo(-34, 248);
-//                ctx.lineTo(0, 0);
-//                ctx.stroke();
-//                ctx.clip();
-//                ctx.drawImage(img, 0, 0, img.width, img.height);
-//                ctx.closePath();
-//                ctx.fillStrokeShape(self);
-//                ctx.restore();
-//
-//            }
-//        },
-//        x: 100,
-//        y: 100,
-//        fill: '#000000',
-//        stroke: '#000000',
-//        strokeWidth: 20
-//    });
-//
-//    layer.add(circle_outer);
-//
-//    stage.add(layer);
-//}
-//
-//for (var i = 0; i < 3; i++) {
-//    insideFor(i);
-//}
-
 
 
