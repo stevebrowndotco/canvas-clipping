a21c42109f2ae0f2032039ecff819cb60c3c0268
Steve Brown <contactstevebrown@gmail.com>
2015-02-12 20:29:57 +0000

    progress!

--- js/triangles.js
+++ js/triangles.js
@@ -9,80 +9,112 @@ var container = document.getElementById('container'),
     iterations = 8;
 
 for (var i = 0; i < iterations; i++) {
-    createTriangleGroup(i)
+
+    createTriangleGroup(i);
+
 }
+;
 
 stage.add(layer);
 
+
+//var triangle = new Kinetic.Shape({
+//
+//    sceneFunc: function (ctx) {
+//        var img = document.createElement('img'),
+//            self = this,
+//            xo = -34,           // x overlap
+//            w = 248,            // triangle width
+//            x = (i * w);    // x position
+//        img.src = 'img/pattern.jpg';
+//
+//        img.onload = function () {
+//            ctx.save();
+//            ctx.beginPath();
+//            ctx.moveTo(0 + x, 0);
+//            ctx.lineTo(207 + x, 140);
+//            ctx.lineTo(-34 + x, w);
+//            ctx.lineTo(0 + x, 0);
+//            ctx.clip();
+//            ctx.drawImage(img, (0 + x) + xo, 0, img.width, img.height);
+//            ctx.stroke();
+//            ctx.closePath();
+//            ctx.fillStrokeShape(self);
+//            ctx.restore();
+//        }
+//    }
+//})
+
+
 function createTriangleGroup(i) {
 
-    var leftTriangle = new Kinetic.Shape({
+    var triangle = function (direction) {
+        return new Kinetic.Shape({
 
-        test: Math.random(),
+            sceneFunc: function (ctx) {
+                var img = document.createElement('img'),
+                    self = this,
+                    xo = -34,           // x overlap
+                    w = 248,            // triangle width
+                    x = (i * w);    // x position
 
-        drawFunc: function (ctx) {
-            var img = document.createElement('img'),
-                self = this,
-                xo = -34,           // x overlap
-                w = 248,            // triangle width
-                x = (i * w);    // x position
-            img.src = 'img/pattern.jpg';
+                if (direction === 'left') {
+                    img.src = 'img/pattern.jpg';
 
-            img.onload = function () {
-                ctx.save();
-                ctx.beginPath();
-                ctx.moveTo(0 + x, 0);
-                ctx.lineTo(207 + x, 140);
-                ctx.lineTo(-34 + x, w);
-                ctx.lineTo(0 + x, 0);
-                ctx.clip();
-                ctx.drawImage(img, (0 + x) + xo, 0, img.width, img.height);
-                ctx.stroke();
-                ctx.closePath();
-                ctx.fillStrokeShape(self);
-                ctx.restore();
+                    img.onload = function () {
+                        ctx.save();
+                        ctx.beginPath();
+                        ctx.moveTo(0 + x, 0);
+                        ctx.lineTo(207 + x, 140);
+                        ctx.lineTo(-34 + x, w);
+                        ctx.lineTo(0 + x, 0);
+                        ctx.clip();
+                        ctx.drawImage(img, (0 + x) + xo, 0, img.width, img.height);
+                        ctx.stroke();
+                        ctx.closePath();
+                        ctx.fillStrokeShape(self);
+                        ctx.restore();
+                    }
+                } else {
+                    var img = document.createElement('img'),
+                        self = this;
+                    img.src = 'img/darth-vader.jpg';
+                    img.onload = function () {
+                        ctx.save();
+                        ctx.beginPath();
+                        ctx.moveTo(0 + x, 115);
+                        ctx.lineTo(243 + x, 0);
+                        ctx.lineTo(208 + x, w);
+                        ctx.lineTo(0 + x, 115);
+                        ctx.clip();
+                        ctx.drawImage(img, (0 + x) + xo, 0, img.width, img.height);
+                        ctx.closePath();
+                        ctx.fillStrokeShape(self);
+                        ctx.restore();
+                    }
+
+                }
             }
-        }
-    })
 
-    .on('mousedown', function (e) {
-        console.log(e.target.attrs.test);
-    })
-    .on('mouseover', function (e) {
-        this.setStroke('red');
-        layer.draw();
-        container.style.cursor = 'pointer';
-    })
-    .on('mouseout', function (e) {
-        this.setStroke('none');
-        layer.draw();
+        })
+        .on('mousedown', function (e) {
+            console.log(e.target._id);
+        })
+        .on('mouseover', function (e) {
+            this.setStroke('red');
+            layer.draw();
+            container.style.cursor = 'pointer';
+        })
+        .on('mouseout', function (e) {
+            this.setStroke('none');
+            layer.draw();
             container.style.cursor = 'auto';
-    });
+        })
 
-    var rightTriangle = new Kinetic.Shape({
-        x: (i * 240),
-        y: i * 140,
-        drawFunc: function (ctx) {
-            var img = document.createElement('img'),
-                self = this;
-            img.src = 'img/darth-vader.jpg';
-            img.onload = function () {
-                ctx.save();
-                ctx.beginPath();
-                ctx.moveTo(0, 115);
-                ctx.lineTo(243, 0);
-                ctx.lineTo(208, 248);
-                ctx.lineTo(0, 115);
-                ctx.clip();
-                ctx.drawImage(img, 0, 0, img.width, img.height);
-                ctx.closePath();
-                ctx.fillStrokeShape(self);
-                ctx.restore();
-            }
-        }
-    });
+    }
 
-    layer.add(leftTriangle, rightTriangle);
+
+    layer.add(triangle('left'), triangle('right'));
 
 };
 
