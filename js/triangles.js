2028edba11ebab5f86e5ac0943d2819e2ea23dd0
Steve Brown <contactstevebrown@gmail.com>
2015-03-14 14:14:04 +0000

    woohoo

--- js/triangles.js
+++ js/triangles.js
@@ -6,13 +6,12 @@ var stage = new Kinetic.Stage({
 
 var container = document.getElementById('container'),
     layer = new Kinetic.Layer(),
-    iterations = 3,
-    triangleHeight = 3;
+    rows = 8;
+    padding = 10,
+    length = 200,
+    iterations = window.innerWidth / (length + padding) + 1;
 
-var shape = function (rows, cols, padding, radius, direction) {
-
-    rows += 1;
-    cols += 1;
+var createShape = function (row, col, padding, length) {
 
     return new Kinetic.Shape({
 
@@ -21,25 +20,33 @@ var shape = function (rows, cols, padding, radius, direction) {
             var img = document.createElement('img'),
                 self = this;
 
+            var xm = ((length + padding)* col),
+                ym = (((length /2) + padding) * row) - ((length / 2));
 
-            var xm = radius * cols,
-                ym = (radius) * rows;
-
+            if(row % 2 === 1) {
+                xm = xm - (length + padding);
+            }
 
             var imgx = xm,
-                imgy = ym,
-                h = ((Math.PI * 2) / 3);
+                imgy = ym;
 
             img.src = 'img/pattern.jpg';
-
 
             img.onload = function () {
 
                 ctx.save();
                 ctx.beginPath();
-                ctx.moveTo(radius * direction + xm, 0 + ym);
-                ctx.lineTo(radius * direction * Math.cos(h) + xm, radius * Math.sin(h) + ym);
-                ctx.lineTo(radius * direction * Math.cos(h * 2) + xm, radius * Math.sin(h * 2) + ym);
+
+                if((col % 2) === 1) {
+                    ctx.moveTo(0 + xm, 0 + ym);
+                    ctx.lineTo(length + xm, length / 2 + ym);
+                    ctx.lineTo(0 + xm, length + ym);
+                } else {
+                    ctx.moveTo(0 + xm, length / 2 + ym);
+                    ctx.lineTo(length + xm, 0 + ym);
+                    ctx.lineTo(length + xm, length + ym);
+                }
+
                 ctx.clip();
                 ctx.drawImage(img, imgx, imgy, img.width, img.height);
                 ctx.closePath();
@@ -74,9 +81,9 @@ var shape = function (rows, cols, padding, radius, direction) {
 
 };
 
-for (var a = 0; a < triangleHeight; a++) {
+for (var a = 0; a < rows; a++) {
     for (var b = 0; b < iterations; b++) {
-        layer.add(shape(a, b, 10, 100, (b % 2) === 1 ? 1 : -1));
+        layer.add(createShape(a, b, padding, length));
     };
 };
 
